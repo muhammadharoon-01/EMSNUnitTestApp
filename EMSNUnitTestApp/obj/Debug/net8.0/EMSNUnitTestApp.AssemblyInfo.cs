0d929e286460d9cfdffd6910c3b2656e7ed889f0
muhammadharoon-01 <79999542+muhammadharoon-01@users.noreply.github.com>
2024-10-30 22:48:44 +0500

    Revert "updated"
    This reverts commit bf558dc7809d2a867bd062c44b9af99ee7a28875.

--- EMSNUnitTestApp/obj/Debug/net8.0/EMSNUnitTestApp.AssemblyInfo.cs
+++ EMSNUnitTestApp/obj/Debug/net8.0/EMSNUnitTestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMSNUnitTestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa3d726e94c11da79f71cd256732f5a170a6597")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMSNUnitTestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMSNUnitTestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
