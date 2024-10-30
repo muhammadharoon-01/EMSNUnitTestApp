515636cd0024fabb8435e39e803179d25cc4d3b5
muhammadharoon-01 <79999542+muhammadharoon-01@users.noreply.github.com>
2024-10-30 22:57:12 +0500

    hghg

--- EMSNUnitTestApp/obj/Debug/net8.0/EMSNUnitTestApp.AssemblyInfo.cs
+++ EMSNUnitTestApp/obj/Debug/net8.0/EMSNUnitTestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMSNUnitTestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d78816727d930367bad57a21b83ff66d7aa8131")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMSNUnitTestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMSNUnitTestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
