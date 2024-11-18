b5274089208953645654b740726fbcabe6fa5d34
muhammadharoon-01 <79999542+muhammadharoon-01@users.noreply.github.com>
2024-11-18 18:10:03 +0500

    Update psexec-script.bat

--- EMSNUnitTestApp/psexec-script.bat
+++ EMSNUnitTestApp/psexec-script.bat
@@ -2,6 +2,6 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p Zaroon7890@ "C:\Program Files (x86)\Smart Wires\SmartInterface\SmartInterface.exe"
+PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p Zaroon7890@ "J:\EMS\Code Workspace\Working\EMSApplication.exe"
 
 endlocal
