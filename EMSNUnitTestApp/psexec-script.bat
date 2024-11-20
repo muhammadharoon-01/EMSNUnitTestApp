9af1bd44fb380514efb014ddcb13c56bf38def38
muhammadharoon-01 <79999542+muhammadharoon-01@users.noreply.github.com>
2024-11-20 21:17:25 +0500

    Cred. added UC system.

--- EMSNUnitTestApp/psexec-script.bat
+++ EMSNUnitTestApp/psexec-script.bat
@@ -2,6 +2,6 @@
 setlocal
 
 REM Use PsExec to launch application as an admin
-PsExec.exe -i 1 -h -d -u POWER\ibrar.sakhi -p Zaroon7890@ "C:\ProgramData\Jenkins\.jenkins\workspace\EMS\EMSNUnitTestApp\bin\Debug\net8.0\EMSApplication.exe"
+PsExec.exe -i 1 -h -d -u POWER\smartwiresatm -p sw@atm "C:\ProgramData\Jenkins\.jenkins\workspace\EMS\EMSNUnitTestApp\bin\Debug\net8.0\EMSApplication.exe"
 
 endlocal
